7cd509de96dc650fe8bff04bc2a6bd4eba5a3618
Yehoslav Rudenco <yehoslavrudenco@protonmail.com>
2026-03-18 13:56:36 +0200

    am formatat fișierele

--- src/routes/tablou/+page.svelte
+++ src/routes/tablou/+page.svelte
@@ -12,33 +12,39 @@
     const nr_intrebare = event.select('nr_intrebare');
     const raspuns = event.select('raspuns');
 
-    const apasat = (event.select('apasat'));
-    const echipa_activa = $derived($apasat ? $apasat : data.echipa_activa.toString())
+    const apasat = event.select('apasat');
+    const echipa_activa = $derived(
+        $apasat ? $apasat : data.echipa_activa.toString(),
+    );
 
     const echipa1_puncte = event.select('puncte1');
     const echipa2_puncte = event.select('puncte2');
 
     const idx_intrebare_activa = $derived(
-        ($nr_intrebare ? parseInt($nr_intrebare) : data.nr_intrebare) % intrebari.length,
+        ($nr_intrebare ? parseInt($nr_intrebare) : data.nr_intrebare) %
+            intrebari.length,
     );
     const intrebarea_activa = $derived(intrebari[idx_intrebare_activa]);
 
     let timp = event.select('timp');
 
-    let secunde = $derived(Math.max(0, parseInt($timp ?? 0)%60));
-    let minute = $derived(Math.max(0, Math.floor(parseInt($timp ?? 0)/60)));
+    let secunde = $derived(Math.max(0, parseInt($timp ?? 0) % 60));
+    let minute = $derived(Math.max(0, Math.floor(parseInt($timp ?? 0) / 60)));
 
-    /** 
+    /**
      * @param {string} text
      * @return {string}
      */
     function pad_left(text) {
-        if (text.length < 2) return '0'+text;
+        if (text.length < 2) return '0' + text;
         return text;
     }
 </script>
+
 <div class="bg-[url(/baza-90.jpg)] bg-cover text-yellow-900">
-    <div class="fixed w-full font-bold flex flex-row justify-between text-5xl bg-amber-100/80 ">
+    <div
+        class="fixed flex w-full flex-row justify-between bg-amber-100/80 text-5xl font-bold"
+    >
         <div
             class="flex w-full flex-row px-15 py-10"
             class:bg-orange-300={$apasat === '1'}
@@ -49,16 +55,19 @@
             </div>
         </div>
 
-        <div
-            class="w-[600px] text-center text-stone-400"
-        >
+        <div class="w-150 text-center text-stone-400">
             <div class="relative py-10">
-                <span>{pad_left(minute.toString())}:{pad_left(secunde.toString())}</span>
+                <span
+                    >{pad_left(minute.toString())}:{pad_left(
+                        secunde.toString(),
+                    )}</span
+                >
 
-
-                <div class="absolute w-full bottom-[-1rem]">
-
-                    <span class="font-bold mx-auto text-emerald-500 w-fit text-center text-3xl bg-emerald-50 py-1 px-2 rounded-xl border-2 border-emerald-100">+{intrebarea_activa.puncte}pt</span>
+                <div class="absolute -bottom-4 w-full">
+                    <span
+                        class="mx-auto w-fit rounded-xl border-2 border-emerald-100 bg-emerald-50 px-2 py-1 text-center text-3xl font-bold text-emerald-500"
+                        >+{intrebarea_activa.puncte}pt</span
+                    >
                 </div>
             </div>
         </div>
@@ -74,31 +83,35 @@
         </div>
     </div>
 
-    <div class="w-full h-[100vh] flex flex-col justify-center text-center text-6xl font-mono font-bold">
+    <div
+        class="flex h-screen w-full flex-col justify-center text-center font-mono text-6xl font-bold"
+    >
         <div class="relative">
-
-            {#if intrebarea_activa.tip === 'emoji' || intrebarea_activa.tip === 'text' }
-                <span 
-                    class:text-9xl={intrebarea_activa.tip === 'emoji'}
-                >{intrebarea_activa.titlu}</span>
+            {#if intrebarea_activa.tip === 'emoji' || intrebarea_activa.tip === 'text'}
+                <span class:text-9xl={intrebarea_activa.tip === 'emoji'}
+                    >{intrebarea_activa.titlu}</span
+                >
             {:else if intrebarea_activa.tip === 'cîntec'}
-                <div class="flex flex-row justify-center gap-10 items-center">
-                    <img height='400px' class="animate-bounce" src="/img/apu_listen_muzik.webp" alt="Apu listens to music">
+                <div class="flex flex-row items-center justify-center gap-10">
+                    <img
+                        height="400px"
+                        class="animate-bounce"
+                        src="/img/apu_listen_muzik.webp"
+                        alt="Apu listens to music"
+                    />
                 </div>
             {:else if intrebarea_activa.tip === 'imagine'}
-                <div class="flex flex-row justify-center gap-10 items-center">
+                <div class="flex flex-row items-center justify-center gap-10">
                     {@html intrebarea_activa.html}
                 </div>
             {:else}
                 <span>{intrebarea_activa.titlu}</span>
             {/if}
-
-
         </div>
     </div>
 
     {#if $raspuns !== ''}
-        <div 
+        <div
             class="fixed bottom-0 w-full"
             class:bg-red-50={$raspuns === 'gresit'}
             class:bg-green-50={$raspuns === 'corect'}
@@ -106,16 +119,15 @@
             <div
                 class:text-red-500={$raspuns === 'gresit'}
                 class:text-green-500={$raspuns === 'corect'}
-                class="w-[1200px] m-auto py-[2.5vh] text-center text-4xl font-bold"
+                class="m-auto w-300 py-[2.5vh] text-center text-4xl font-bold"
             >
                 Raspuns {$raspuns}
-                {#if $raspuns === "corect"}
+                {#if $raspuns === 'corect'}
                     : {intrebarea_activa.raspuns}
                 {/if}
             </div>
         </div>
     {/if}
-
 </div>
 
 <style>
